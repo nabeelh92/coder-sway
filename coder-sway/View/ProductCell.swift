4083a1f84291be3b91a42acf2acb1a22c7a54ccb
nabeelh92 <nabeelhuq@gmail.com>
2017-10-21 15:10:52 -0700

    added collectionView with custom cell

---
+++ coder-sway/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-sway
+//
+//  Created by Nabeel Huq on 2017-10-21.
+//  Copyright © 2017 Nabeel Huq. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
