1ac4d4eb42904a1e4b2e1074efc2b9b34dfb0deb
nabeelh92 <nabeelhuq@gmail.com>
2017-10-21 14:11:06 -0700

    getting started with first view

---
+++ coder-sway/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-sway
+//
+//  Created by Nabeel Huq on 2017-10-21.
+//  Copyright © 2017 Nabeel Huq. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
