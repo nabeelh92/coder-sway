a678a03daa66fd06836053194eec4cafff5f1ba6
nabeelh92 <nabeelhuq@gmail.com>
2017-10-21 20:10:28 -0700

    clicking on an item now goes to the VC and populates the collection view

--- coder-sway/Controller/CategoriesVC.swift
+++ coder-sway/Controller/CategoriesVC.swift
@@ -33,6 +33,24 @@ class CategoriesVC: UIViewController, UITableViewDelegate, UITableViewDataSource
             return CategoryCell()
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            
+            assert(sender as? Category != nil)
+            productsVC.initProdcuts(category: sender as! Category)
+           
+        }
+    }
 
 }
 
